26b6bebcf17545087dc76c4ff44aaea63a8bfdfa
Spikade <147817997+Spikade@users.noreply.github.com>
2023-12-07 20:21:31 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 export const images = [
   {
-    path: "./assets/imgs1/01.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/01.jpg",
     position: { top: "-5%", left: "5%" },
     speed: 0.08,
   },
   {
-    path: "./assets/imgs1/02.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/02.jpg",
     position: { top: "25%", left: "20%" },
     speed: 0.03,
   },
   {
-    path: "./assets/imgs1/03.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/03.jpg",
     position: { top: "60%", left: "40%" },
     speed: 0.065,
   },
   {
-    path: "./assets/imgs1/04.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/04.jpg",
     position: { top: "70%", left: "10%" },
     speed: 0.04,
   },
   {
-    path: "./assets/imgs1/05.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/05.jpg",
     position: { top: "-10%", left: "65%" },
     speed: 0.025,
   },
   {
-    path: "./assets/imgs1/06.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/06.jpg",
     position: { top: "10%", left: "85%" },
     speed: 0.05,
   },
   {
-    path: "./assets/imgs1/07.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/07.jpg",
     position: { top: "40%", left: "60%" },
     speed: 0.025,
   },
   {
-    path: "./assets/imgs1/08.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/08.jpg",
     position: { top: "80%", left: "70%" },
     speed: 0.1,
   },
   {
-    path: "./assets/imgs1/09.jpg",
+    path: "https://spikade.github.io/XVERSE/assets/imgs1/09.jpg",
     position: { top: "40%", left: "-5%" },
     speed: 0.065,
   },
